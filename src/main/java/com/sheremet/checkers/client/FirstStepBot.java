1c55ca7ed98304cd5bbc826616376325a3293aa2
yarphen <leviatan.ka14@gmail.com>
2016-12-12 03:58:44 +0200

    Simple bot fixing for multisteps

--- src/main/java/com/sheremet/checkers/client/FirstStepBot.java
+++ src/main/java/com/sheremet/checkers/client/FirstStepBot.java
@@ -10,12 +10,13 @@ import checkers.pojo.board.Numbers;
 import checkers.pojo.checker.Checker;
 import checkers.pojo.checker.CheckerColor;
 import checkers.pojo.checker.Position;
+import checkers.pojo.exceptions.DoOneMoreStepException;
 import checkers.pojo.step.Step;
 import checkers.pojo.step.StepUnit;
 import checkers.utils.Validator;
 /**
-* Created by mykhaylo sheremet on 11.12.2016.
-*/
+ * Created by mykhaylo sheremet on 11.12.2016.
+ */
 public class FirstStepBot implements CheckersBot{
 	private CheckerColor color;
 	private String name;
@@ -40,14 +41,49 @@ public class FirstStepBot implements CheckersBot{
 						try{
 							Position p = new Position(x, y);
 							StepUnit stepUnit = new StepUnit(checker.getPosition(), p);
-							Step step = new Step();
+							Step step = finalStep!=null?finalStep:new Step();
 							step.addStep(stepUnit);
 							if (new Validator().isValidStep(board, step, color)){
-								board.apply(step);
-								if (true){
-									finalStep = step;
-									break outer;
+								try{
+									board.apply(step);
+								}catch(IllegalArgumentException e){
+									if (e.getCause() instanceof DoOneMoreStepException){
+										inner:while(true){
+											for(int x1=1; x1<=8; x1++){
+												for(int y1=1; y1<=8; y1++){
+													if ((x+y)%2==0){
+														try{
+															Position p1 = new Position(x1, y1);
+															StepUnit stepUnit1 = new StepUnit(step.getSteps().get(step.getSteps().size()-1).getTo(), p1);
+															step.addStep(stepUnit1);
+															if (new Validator().isValidStep(board, step, color)){
+																try{
+																	board.apply(step);
+																	break inner;
+																}catch(IllegalArgumentException e2){
+																	if (e2.getCause() instanceof DoOneMoreStepException){
+																		continue inner;
+																	}else{
+																		throw e2;
+																	}
+																}
+															}else{
+																step.getSteps().remove(step.getSteps().size()-1);
+															}
+														}catch(IllegalArgumentException e2){
+															step.getSteps().remove(step.getSteps().size()-1);
+														}
+													}
+												}
+											}
+											break outer;
+										}
+									}else{
+										throw e;
+									}
 								}
+								finalStep = step;
+								break outer;
 							}
 						}catch(IllegalArgumentException e){ }
 					}
